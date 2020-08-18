add1cb0f5df694de424f1f1a5dfdb76215a9e06e
jeasung-pf <jeasung@peoplefund.co.kr>
2020-08-18 11:27:47 +0900

    First Commit

---
+++ src/main/java/tech/peoplefund/iekg/IekgApplication.java
@@ -0,0 +1,13 @@
+package tech.peoplefund.iekg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IekgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IekgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/peoplefund/iekg/IekgApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.peoplefund.iekg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IekgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
